a8d92e9fc7280b34941c3af118792de1495cdc2a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-03-08 23:21:52 +1300

    Fix virtual host setup.

--- falcon.rb
+++ falcon.rb
@@ -3,20 +3,21 @@
 
 # Copyright, 2025, by Samuel Williams.
 
-require "falcon/environment/self_signed_tls"
-require "falcon/environment/rack"
-require "falcon/environment/supervisor"
+require 'falcon/environment/rack'
+require 'falcon/environment/tls'
+require 'falcon/environment/lets_encrypt_tls'
+require 'falcon/environment/supervisor'
 
-service "chick.nz" do
-	# include Falcon::Environment::SelfSignedTLS
+service 'supervisor' do
+	include Falcon::Environment::Supervisor
+end
+
+hostname = File.basename(__dir__)
+
+service hostname do
 	include Falcon::Environment::Rack
-	
-	scheme "http"
-	protocol {Async::HTTP::Protocol::HTTP}
-	
-	endpoint do
-		Async::HTTP::Endpoint.for(scheme, "localhost", port: 9292, protocol: protocol)
-	end
+	include Falcon::Environment::TLS
+	include Falcon::Environment::LetsEncryptTLS
 end
 
 require_relative "lib/camcast/service/environment"
